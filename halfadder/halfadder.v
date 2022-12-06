933acbf50eb362b4faacddc1e6d1839c0c5c3929
BhargavRajyaguru <119412595+BhargavRajyaguru@users.noreply.github.com>
2022-12-06 16:26:43 +0530

    Init

---
+++ halfadder/halfadder.v
@@ -0,0 +1,9 @@
+module HF(sum,carry,a,b);
+
+  output sum, carry;
+  input a, b;
+
+  assign sum = a ^ b; // assigning sum
+  assign carry = a & b; // assigning carry
+
+endmodule
---
+++ halfadder/halfadder_tb.v
@@ -0,0 +1,22 @@
+module haldadder_tb();
+
+  reg  a, b;
+  wire  sum, carry;
+
+  haldadder uut(sum,carry,a,b);
+
+  initial
+    begin
+      a = 0; b =0;
+  #5; a = 0; b =1;
+  #5; a = 1; b =0;
+  #5; a = 1; b =1;
+  #5;
+    end
+
+ //Dump waves (only required here)
+initial
+  begin
+    $dumpfile("dump.vcd");
+    $dumpvars(1);
+  end
